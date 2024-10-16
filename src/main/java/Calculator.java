a318e0b9040f1099ac1f972fa43907adeb5cdf20
Michal Skrzypczynski <276157@student.pwr.edu.pl>
2024-10-16 12:27:47 +0200

    Added subtraction function to Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public static int addition(int a, int b){
         return a + b;
     }
+
+    public static int subtraction(int a, int b){
+        return a - b;
+    }
 }
