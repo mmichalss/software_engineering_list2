6d0446dcde1a2699d1c5e4609e844322579b030e
Michal Skrzypczynski <276157@student.pwr.edu.pl>
2024-10-16 12:35:18 +0200

    Added division function to Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -10,4 +10,8 @@ public class Calculator {
     public static int multiplication(int a, int b){
         return a * b;
     }
+
+    public static double division(int a, int b){
+        return (double) a/b;
+    }
 }
